c5f45b3ffe7e5a1ab695d1356a0f98f3592f4d5c
AzureAD\SatjaSivcev <satja.sivcev@zivid.com>
2019-04-07 11:46:00 +0200

    Initial commit finalized

--- SampleReadZDF.m
+++ SampleReadZDF.m
@@ -1,24 +1,25 @@
-%%%
+% This example shows how to import a Zivid point cloud from a .ZDF file.
+
 % Copyright (c) 2019, Zivid AS
-% 
+%
 % All rights reserved.
-% 
+%
 % Redistribution and use in source and binary forms, with or without
 % modification, are permitted provided that the following conditions are
 % met:
-% 
+%
 % 1. Redistributions of source code must retain the above copyright
 %    notice, this list of conditions and the following disclaimer.
-% 
+%
 % 2. Redistributions in binary form must reproduce the above copyright
 %    notice, this list of conditions and the following disclaimer in the
 %    documentation and/or other materials provided with the
 %    distribution.
-% 
+%
 % 3. Neither the name of Zivid AS nor the names of its contributors may
 %    be used to endorse or promote products derived from this software
 %    without specific prior written permission.
-% 
+%
 % THIS SOFTWARE IS PROVIDED BY ZIVID AS AND CONTRIBUTORS "AS IS" AND ANY
 % EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 % IMPLIED WARRANTIES OF MERCHANTABILITY, AND FITNESS FOR A PARTICULAR
@@ -30,21 +31,25 @@
 % WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 % OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 % IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-%%%
-
-%%%
-% This example shows how to import a Zivid point cloud from a .ZDF file.
-%%%
 
 %Read a .ZDF point cloud. The "Zivid3D.zdf" file has to be in the same folder as the "SampleReadZDF" file.
 Filename = 'Zivid3D.zdf';
 [pc,X,Y,Z,R,G,B,Image,Contrast] = zdfread(Filename);
 
 %Display the RGB image
-imshow(Image);
+figure('units','normalized','outerposition',[0 0 1 1])
+imagesc(Image);
+pbaspect([1920 1200 1])
 
 %Display the Z component
+figure('units','normalized','outerposition',[0 0 1 1])
 imagesc(Z);
+pbaspect([1920 1200 1])
+colormap jet
+colorbar
 
 %Visualize the point cloud
+figure('units','normalized','outerposition',[0 0 1 1])
 pcshow(pc);
+view([0 -90]);
+set(gca, 'visible', 'off')
--- zdfread.m
+++ zdfread.m
@@ -1,24 +1,45 @@
-%%%
+function [pc, X, Y, Z, R, G, B, im, contrast] = zdfread(filename)
+
+% [pc, X, Y, Z, R, G, B, im, contrast, raw] = zdfreadpublic(filename)
+%
+% Simplified function for reading ZDF files
+%
+% INPUT:
+% filename - Full folder and filename to ZDF file
+%
+% OUTPUT:
+% pc - Pointcloud (for use by pcshow and other MATLAB specific functions)
+% X - x data in a matrix
+% Y - y data in a matrix
+% Z - z data in a matrix
+% R - Red color band from color image (scaled from 0 to 1)
+% G - Green color band from color image (scaled from 0 to 1)
+% B - Blue color band from color image (scaled from 0 to 1)
+% im - Color image (uint8 - 0 to 255)
+% contrast - Contrast image / quality image (float)
+
+% Author: Martin Ingvaldsen
+
 % Copyright (c) 2019, Zivid AS
-% 
+%
 % All rights reserved.
-% 
+%
 % Redistribution and use in source and binary forms, with or without
 % modification, are permitted provided that the following conditions are
 % met:
-% 
+%
 % 1. Redistributions of source code must retain the above copyright
 %    notice, this list of conditions and the following disclaimer.
-% 
+%
 % 2. Redistributions in binary form must reproduce the above copyright
 %    notice, this list of conditions and the following disclaimer in the
 %    documentation and/or other materials provided with the
 %    distribution.
-% 
+%
 % 3. Neither the name of Zivid AS nor the names of its contributors may
 %    be used to endorse or promote products derived from this software
 %    without specific prior written permission.
-% 
+%
 % THIS SOFTWARE IS PROVIDED BY ZIVID AS AND CONTRIBUTORS "AS IS" AND ANY
 % EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 % IMPLIED WARRANTIES OF MERCHANTABILITY, AND FITNESS FOR A PARTICULAR
@@ -30,78 +51,49 @@
 % WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 % OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 % IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-%%%
 
-function [pc, X, Y, Z, R, G, B, im, contrast] = zdfread(filename)
-% [pc, X, Y, Z, R, G, B, im, contrast, raw] = zdfreadpublic(filename)
-% 
-% Simplified function for reading ZDF files
-%
-% INPUT:
-% filename - Full folder and filename to ZDF file
-%
-% OUTPUT: 
-% pc - Pointcloud (for use by pcshow and other MATLAB specific functions)
-% X - x data in a matrix
-% Y - y data in a matrix
-% Z - z data in a matrix
-% R - Red color band from color image (scaled from 0 to 1)
-% G - Green color band from color image (scaled from 0 to 1)
-% B - Blue color band from color image (scaled from 0 to 1)
-% im - Color image (uint8 - 0 to 255)
-% contrast - Contrast image / quality image (float)
-%
+pc = [];
+X = [];
+Y = [];
+Z = [];
+R = [];
+G = [];
+B = [];
+im = [];
+contrast = [];
 
-% Author: Martin Ingvaldsen
-% (c) Zivid Labs, 2017
-
-    pc = [];
-    X = [];
-    Y = [];
-    Z = [];
-    R = [];
-    G = [];
-    B = [];
-    im = [];
-    contrast = [];
-
-    try
-        % Read color image from rgba_image
-        % and split in R, G, B normalized matrices
-        imo = ncread(filename,'data/rgba_image');
-        imo = permute(imo,[3 2 1]);
-        for i=1:3
-            im(:,:,i) = imo(:,:,i);
-        end
-
-        R = double(im(:,:,1)) / 255;
-        G = double(im(:,:,2)) / 255;
-        B = double(im(:,:,3)) / 255;
-
-        im = uint8(im);   
-    end
-
-    try
-        % read pointcloud data, create pointcloud class and
-        % X, Y, Z matrices
-        pco = ncread(filename,'data/pointcloud');
-        pco = permute(pco,[3 2 1]);
-
-        X = pco(:,:,1);
-        Y = pco(:,:,2);
-        Z = pco(:,:,3);
-        XYZ(:,:,1) = X;
-        XYZ(:,:,2) = Y;
-        XYZ(:,:,3) = Z;
-        if ~isempty(im)
-            pc=pointCloud(XYZ,'color',double(im)./255);
-        end
+try
+    % Read color image from rgba_image
+    % and split in R, G, B normalized matrices
+    imo = ncread(filename,'data/rgba_image');
+    imo = permute(imo,[3 2 1]);
+    for i=1:3
+        im(:,:,i) = imo(:,:,i);
     end
     
-    try
-        % Read the contrast / quality data
-        contrast = ncread(filename,'data/contrast')';
+    R = double(im(:,:,1)) / 255;
+    G = double(im(:,:,2)) / 255;
+    B = double(im(:,:,3)) / 255;
+    
+    im = uint8(im);
+    
+    % read pointcloud data, create pointcloud class and
+    % X, Y, Z matrices
+    pco = ncread(filename,'data/pointcloud');
+    pco = permute(pco,[3 2 1]);
+    
+    X = pco(:,:,1);
+    Y = pco(:,:,2);
+    Z = pco(:,:,3);
+    XYZ(:,:,1) = X;
+    XYZ(:,:,2) = Y;
+    XYZ(:,:,3) = Z;
+    if ~isempty(im)
+        pc=pointCloud(XYZ,'color',double(im)./255);
     end
-
+    
+    % Read the contrast / quality data
+    contrast = ncread(filename,'data/contrast')';
 end
 
+end
