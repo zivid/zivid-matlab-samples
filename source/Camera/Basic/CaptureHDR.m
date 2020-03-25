4850a1865f6e9394f07e32dc6c46409099b84205
AzureAD\SatjaSivcev <satja.sivcev@zivid.com>
2020-03-25 14:00:15 +0100

    Fix CaptureHDR - app.ConnectCamera, print text, add camera.Disconnect

--- source/Camera/Basic/CaptureHDR.m
+++ source/Camera/Basic/CaptureHDR.m
@@ -2,14 +2,14 @@ try
     app = zividApplication;
 
     disp('Connecting to camera')
-    camera = connectCamera(app);
+    camera = app.ConnectCamera;
 
     disp('Recording HDR source images');
     frames = NET.createGeneric('System.Collections.Generic.List',{'Zivid.NET.Frame'});
 
     settings = camera.Settings;
     for iris = [14 21 35]
-        disp(['Capture frame with iris = ' num2str(iris)]);
+        disp(['Capturing frame with iris = ' num2str(iris)]);
         settings.Iris = iris;
         camera.SetSettings(settings)
         frames.Add(camera.Capture());
@@ -18,11 +18,14 @@ try
     disp('Creating HDR frame');
     hdrFrame = Zivid.NET.HDR.CombineFrames(frames);
 
-    disp('Saving the frames')
-    frame(1).Save('14.zdf');
-    frame(1).Save('21.zdf');
-    frame(1).Save('35.zdf');
+    disp('Saving framese to file:');
+    frames.Item(0).Save('14.zdf');
+    frames.Item(1).Save('21.zdf');
+    frames.Item(2).Save('35.zdf');
     hdrFrame.Save('HDR.zdf');
+    
+    disp('Disconnecting from camera')
+    camera.Disconnect;
 
 catch ex
 
