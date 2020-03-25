bcfb942c9ad2315563bce4fa334ebcd7a12f7058
AzureAD\SatjaSivcev <satja.sivcev@zivid.com>
2020-03-25 13:56:37 +0100

    Fix CaptureAssistant - app/ConnectCamera, print text, add camera.Disconnect

--- source/Camera/Basic/CaptureAssistant.m
+++ source/Camera/Basic/CaptureAssistant.m
@@ -2,23 +2,26 @@ try
     app = zividApplication; 
 
     disp('Connecting to camera')
-    camera = connectCamera(app);
+    camera = app.ConnectCamera;
     
     suggestSettingsParameters = Zivid.NET.('CaptureAssistant+SuggestSettingsParameters')(Zivid.NET.Duration.FromMilliseconds(1200),Zivid.NET.('CaptureAssistant+AmbientLightFrequency.none'));
     disp(['Running Capture Assistant with parameters: ', char(suggestSettingsParameters.ToString())]);
     settingsList = Zivid.NET.CaptureAssistant.SuggestSettings(camera, suggestSettingsParameters);
     
-    disp('Suggested settings are:');
+    disp('Suggested settings:');
     for i = 0:settingsList.Count-1
         disp(settingsList.Item(i).ToString);
     end
     
-    disp('Capture (and merge) frames using automatically suggested settings');
+    disp('Capturing (and merge) frames using automatically suggested settings');
     hdrFrame = Zivid.NET.HDR.Capture(camera, settingsList);
     
     resultFile = 'result.zdf';
     disp(['Saving frame to file: ' resultFile]);
     hdrFrame.Save(resultFile);
+    
+    disp('Disconnecting from camera')
+    camera.Disconnect;
 
 catch ex
 
